a38aed1383b44bab2a604ec7a7f63aba8208e42e
Your Name <your.email@example.com>
2026-04-23 13:14:29 +0530

    final deploy ready

--- server/index.js
+++ server/index.js
@@ -13,9 +13,11 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:3000',
+  credentials: true,
 }));
 
+app.options('*', cors()); // ✅ IMPORTANT
 app.use(express.json({ limit: '50mb' }));
 
 // Routes
@@ -54,3 +56,8 @@ if (process.env.NODE_ENV === 'production') {
     res.send('API is running...');
   });
 }
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
